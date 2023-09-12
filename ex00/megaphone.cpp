fdb2c112b6badfb81097937b3ad4e1c2f5ab2ed3
nunom4chado <nunom4chado@gmail.com>
2023-09-12 17:14:28 +0100

    add ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: numartin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 23:07:18 by numartin          #+#    #+#             */
+/*   Updated: 2023/09/12 15:30:04 by numartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            for (int j = 0; argv[i][j]; j++)
+                std::cout << char(toupper(argv[i][j]));
+        }
+        std::cout << std::endl;
+    }
+    return (0);
+}
